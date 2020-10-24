eb2c0c7d8280a01d7074e9717ba11d561a729d49
Harsh <harshsharma102000@gmail.com>
2020-10-24 11:01:59 +0530

    Linkedin Updated

--- pasport.js
+++ pasport.js
@@ -165,8 +165,8 @@ app.get('/auth/facebook/callback',
 // LINKEDIN LOGIN
 
 passport.use(new LinkedInStrategy({
-    clientID: process.env.LINKEDIN_APP_ID,
-    clientSecret: process.env.LINKEDIN_APP_SECRET,
+    clientID: process.env.LINKEDIN_CLIENT_ID,
+    clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
     callbackURL: process.env.LINKEDIN_CALLBACK_URL,
     scope: ['r_emailaddress', 'r_liteprofile'],
 }, function (token, tokenSecret, profile, done) {
